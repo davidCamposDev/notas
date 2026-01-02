de3d39dcf1a65ae728a001c7c78cd29b157c562e
David Campos <davi.alencar144@gmail.com>
2026-01-02 17:30:24 -0400

    feature comands ListNotas e Menu

--- bin/notas.dart
+++ bin/notas.dart
@@ -2,12 +2,15 @@ import 'dart:io';
 
 void main() {
   List<String> notas = <String>[];
-  getComando();
-  adicionarNota(notas);
+  menu(notas);
 }
 
+//Função Capturar comando
+
 String getComando() {
-  print("Digite um Comando: 1 -  Adicionar Nota, 2 - Listar notas, 3 - Sair");
+  print(
+    "\nDigite um Comando:\n 1 -  Adicionar Nota\n 2 - Listar notas\n 3 - Sair",
+  );
   List<String> comandos = <String>["1", "2", "3"];
   String? entrada = "";
 
@@ -21,19 +24,48 @@ String getComando() {
   return entrada!;
 }
 
+//Função Adicionar Notas
+
 List<String> adicionarNota(List<String> notas) {
-  print("Escreva uma Nota!");
+  print("\nEscreva uma Nota!");
 
   String? nota = "";
 
   nota = stdin.readLineSync();
 
   if (nota == null || nota.isEmpty) {
-    print("Não é possivel adiconar uma nota vazia");
+    print("\nNão é possivel adiconar uma nota vazia");
     adicionarNota(notas);
   }
 
   notas.add(nota!);
 
   return notas;
+}
+
+//Função Lista Notas
+
+void listarNotas(List<String> notas) {
+  for (var i = 0; i < notas.length; i++) {
+    print(notas[i]);
+  }
+}
+
+//função Menu
+
+void menu(List<String> notas) {
+  String comando = getComando();
+
+  switch (comando) {
+    case "1":
+      adicionarNota(notas);
+      menu(notas);
+
+    case "2":
+      listarNotas(notas);
+      menu(notas);
+
+    case "3":
+      print("\nAté Breve!");
+  }
 }
